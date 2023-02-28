902e9e4a9a301550bd87f8aba9f27d2a99d2abb1
Shawn Fladager <shawnfladager@gmail.com>
2023-02-28 14:23:29 +0700

    added login instructions and change login info

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -21,6 +21,13 @@ const Home = () => {
               <Button className="button-blue" variant="primary">Sign Up</Button>
             </Link>
           </div>
+          <div className="login-instructions">
+            <p>Login with the following information below if you want to explore a demo account with existing trade information, or create your own account and add your own trades.</p>
+            <p>To start with a new account, add a trade first, then add your executions, and then create a session to journal the trade.</p>
+            <h2>Login info:</h2>
+            <p>email: demo@yahoo.com</p>
+            <p>password: TestPass123!</p>
+          </div>
         </div>
         
       </Container>
